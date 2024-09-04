cd3f3242512788001f62fd59075fb1c8fa82c424
mahyar <mahyar_066@yahoo.com>
2024-09-05 01:11:10 +0330

    Added First urls and views

--- sabzweb/blog/models.py
+++ sabzweb/blog/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+
+# Define Custom Manager (optional) to filter queryset of published posts
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
 # Create your models here.
 class Post(models.Model):
     # Define the class for choice text
@@ -11,7 +18,7 @@ class Post(models.Model):
         REJECTED = 'R', 'Rejected'
 
     # Relations (user to posts)
-    auther = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_posts')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_posts')
 
     # Adding database fields
     title = models.CharField(max_length=250)
@@ -23,6 +30,10 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     # Choices Field
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+
+    # instantiate the custom manager for published posts
+    objects = models.Manager()
+    published = PublishedManager()
 
     # Sorting based on publish field using Meta class and its indexing
     class Meta:
--- sabzweb/blog/views.py
+++ sabzweb/blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def post_list(request):
+    return HttpResponse("Hello, world. You're at the post_list.")
+
+
+def post_detail(request, pk):
+    return HttpResponse(f"Hello, world. You're at the post_detail view, Post:{pk}", pk)
